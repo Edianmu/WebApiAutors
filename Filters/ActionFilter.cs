465197a5b7d845056e34762fc7035e02eb629d2f
Edisson Andrés Muñoz Villegas <edmunoz@sistecredito.com>
2022-08-31 11:16:52 -0500

    Version simple

---
+++ Filters/ActionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApiAutors.Filters
+{
+    public class ActionFilter : IActionFilter
+    {
+        private readonly ILogger<ActionFilter> logger;
+
+        public ActionFilter(ILogger<ActionFilter> logger)
+        {
+            this.logger = logger;
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            logger.LogInformation("Antes de ejecutar la acción");
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            logger.LogInformation("Después de ejecutar la acción");
+        }
+
+
+    }
+}
